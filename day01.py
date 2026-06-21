adf21e0f3e14c8c81022d450f79e8b3da7d30c55
Edward <mredwardsith@gmail.com>
2026-06-21 05:05:19 -0700

    Day 1: variables, f-strings, if/else overtime calculation

---
+++ day01.py
@@ -0,0 +1,13 @@
+name = "Edward"
+age = 30
+rate = 150.00
+print(f"Hi, my name is {name}. I am {age} years old, and my hourly rate is {rate}.")
+
+hours_worked = 45
+hourly_rate = 150.00
+
+if hours_worked > 40: 
+    print((40*hourly_rate)+((hours_worked-40)*hourly_rate*1.5))
+else: 
+    print(hours_worked*hourly_rate)
+
